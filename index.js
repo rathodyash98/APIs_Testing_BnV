e3cc39455eb0c31e9f384794d3a6b2acb6342b67
ZyanHere <133707817+ZyanHere@users.noreply.github.com>
2024-08-05 23:30:19 +0530

    complete frontend updated

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+server.listen(PORT,() => {
+    console.log("Server running on port",PORT);
+})
