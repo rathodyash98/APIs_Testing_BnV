7a23c6650d590e783c67bc7d94b357f7fd58e597
ZyanHere <133707817+ZyanHere@users.noreply.github.com>
2024-12-24 20:43:04 +0530

    new app setup

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import orderRoutes from "./routes/orderRoutes.js";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Connect to Database
+connectDB();
+
+// Routes
+app.use("/api", orderRoutes);
+
+export default app;
